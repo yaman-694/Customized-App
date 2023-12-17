e76e1c22740fef7a8951bed11f11075b20aeb849
harsha-krupashankar <75800001+harsha-krupashankar@users.noreply.github.com>
2023-12-17 19:58:24 +0530

    Added keys

--- src/components/jobs/JobCards.tsx
+++ src/components/jobs/JobCards.tsx
@@ -5,59 +5,64 @@ import { useJobContext } from "@/contexts/jobContext";
 import { JobCard } from "./JobCard";
 
 export function JobCards() {
-  const {
-    state: { searchJobs, loading },
-  } = useJobContext();
-  const jobs = searchJobs;
+    const {
+        state: { searchJobs, loading },
+    } = useJobContext();
+    const jobs = searchJobs;
 
-  const handleClick = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    // Check if the clicked element is a JobCard or its child
-    const jobCardElement = (event.target as Element).closest(".job-card");
+    const handleClick = (
+        event: React.MouseEvent<HTMLDivElement, MouseEvent>
+    ) => {
+        // Check if the clicked element is a JobCard or its child
+        const jobCardElement = (event.target as Element).closest(".job-card");
 
-    if (jobCardElement) {
-      // If it's a JobCard, get the id attribute
-      const jobId = jobCardElement.getAttribute("id");
-    }
-  };
+        if (jobCardElement) {
+            // If it's a JobCard, get the id attribute
+            const jobId = jobCardElement.getAttribute("id");
+        }
+    };
 
-  return (
-    <div className="h-screen">
-      <div className="flex flex-col mt-7">
-        {loading && (
-          <div className="flex justify-center items-center">
-            <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-2 border-gray-900"></div>
-          </div>
-        )}
-        {!loading && jobs?.length === 0 && (
-          <div className="flex justify-center items-center">
-            <div className="text-2xl font-semibold text-gray-900">
-              No jobs found
+    return (
+        <div className="h-screen">
+            <div className="flex flex-col mt-7">
+                {loading && (
+                    <div className="flex justify-center items-center">
+                        <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-2 border-gray-900"></div>
+                    </div>
+                )}
+                {!loading && jobs?.length === 0 && (
+                    <div className="flex justify-center items-center">
+                        <div className="text-2xl font-semibold text-gray-900">
+                            No jobs found
+                        </div>
+                    </div>
+                )}
+                {!loading && jobs && jobs.length > 0 && (
+                    <>
+                        {jobs.map((job) => (
+                            <div className="mb-3" key={job.job_id}>
+                                <Link href={`/${job.job_slug}`}>
+                                    <JobCard
+                                        className="job-card"
+                                        id={job.job_slug} // Set the 'id' attribute here
+                                        key={job.job_id}
+                                        jobName={{
+                                            name: job.name,
+                                            font: "text-3xl",
+                                            fontWeight: "font-semibold",
+                                            fontColor: "text-black",
+                                        }}
+                                        jobDescription={{
+                                            description:
+                                                job.custom_fields[0].value,
+                                        }}
+                                    />
+                                </Link>
+                            </div>
+                        ))}
+                    </>
+                )}
             </div>
-          </div>
-        )}
-        {!loading && jobs && jobs.length > 0 && (
-          <>
-            {jobs.map((job) => (
-              <div className="mb-3" key={job.job_id}>
-                <Link href={`/${job.job_slug}`}>
-                  <JobCard
-                    className="job-card"
-                    id={job.job_slug} // Set the 'id' attribute here
-                    key={job.job_id}
-                    jobName={{
-                      name: job.name,
-                      font: "text-3xl",
-                      fontWeight: "font-semibold",
-                      fontColor: "text-black",
-                    }}
-                    jobDescription={job.description}
-                  />
-                </Link>
-              </div>
-            ))}
-          </>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
--- src/services/jobService.ts
+++ src/services/jobService.ts
@@ -1,4 +1,4 @@
-'use server';
+"use server";
 
 import { FilterBox } from "@/contexts/filterContext";
 
@@ -6,32 +6,33 @@ const API = "https://solutions-test.recruitcrm.io";
 const KEY = "37c5b338-7063-4047-8941-f01ff507b2f3";
 
 export const getJobs = async () => {
-  
-  const response = await fetch(`${API}/jobs/${KEY}`);
-  const data = await response.json();
-  return data;
+    const response = await fetch(`${API}/jobs/${KEY}?keys=custom_field[2]`);
+    const data = await response.json();
+    return data;
 };
 
 export const searchJobs = async ({ filter }: { filter: FilterBox }) => {
-  let query = "";
-  if(filter.jobName) {
-    query += `search_keyword=${filter.jobName}&`;
-  }
-  if(filter.city?.length) {
-    query += `city=${filter.city.join(",")}&`;
-  }
-  if(filter.country?.length) {
-    query += `country=${filter.country.join(",")}`;
-  }
-  console.log(query);
-  const response = await fetch(`${API}/jobs/search/${KEY}?${query}`);
-  const data = await response.json();
-  return data;
+    let query = "";
+    if (filter.jobName) {
+        query += `search_keyword=${filter.jobName}&`;
+    }
+    if (filter.city?.length) {
+        query += `city=${filter.city.join(",")}&`;
+    }
+    if (filter.country?.length) {
+        query += `country=${filter.country.join(",")}`;
+    }
+    console.log(query);
+    const response = await fetch(
+        `${API}/jobs/search/${KEY}?keys=custom_field[2]${query}`
+    );
+    const data = await response.json();
+    return data;
 };
 
 export const getJobBySlug = async (slug: string) => {
-  const response = await fetch(`${API}/jobs/by-slug/${KEY}?job_slug=${slug}`);
-  const data = await response.json();
-  console.log(data);
-  return data;
-}
+    const response = await fetch(`${API}/jobs/by-slug/${KEY}?job_slug=${slug}`);
+    const data = await response.json();
+    console.log(data);
+    return data;
+};
